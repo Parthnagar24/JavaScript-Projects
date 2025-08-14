dce03f197fcc3c055d0178305887265b54ddbd91
Parth Nagar <parthnagar2406@gmail.com>
2025-08-14 23:37:44 +0530

    Create script.js

---
+++ My-Portfolio/script.js
@@ -0,0 +1,7 @@
+// Typed.js for rotating titles
+var typed = new Typed('#element', {
+    strings: ['Full Stack Developer', 'AI/ML Enthusiast'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
